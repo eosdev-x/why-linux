b7bc406892ecd4576c529433f94674c9141e5084
eosdev-x <eosdev.x@gmail.com>
2025-02-23 02:25:53 -0800

    Added "Tux Street" to footer

--- src/App.tsx
+++ src/App.tsx
@@ -135,7 +135,7 @@ function App() {
 
         {/* Footer */}
 <footer className="py-8 px-4 text-center text-gray-600 dark:text-dracula-comment">
-  <p>© {new Date().getFullYear()} Why Linux? All rights reserved.</p>
+  <p>© {new Date().getFullYear()} Tux Street - All rights reserved.</p>
   <p>This site was inspired by my favorite site <a style={{color:"#50FA7B"}} href="https://distrowatch.com/" target="_blank" rel="noopener noreferrer">Distrowatch</a>.</p>
 </footer>
       </div>
